3af54c65b9aa6055af1ec5bf2b8518ee8c5ac12d
desktop <desktop@desktop.com>
2018-04-25 14:34:25 +0800

    Initial Commit

---
+++ PushNotification10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushNotification10
+//
+//  Created by KaiChieh on 2018/4/25.
+//  Copyright © 2018 KaiChieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
